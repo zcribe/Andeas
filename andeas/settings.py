e44f7bb60ffe5812088cbace7956b5fa3a0000b0
zcribe <erlend.eelmets@gmail.com>
2019-10-03 15:56:31 +0300

    Settings file edit

--- andeas/settings.py
+++ andeas/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a7$*(5#evz0sd=y=j)y&*^&m9ald=ue4^&#y_oynlmn+@7m+nc'
+SECRET_KEY = '-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
